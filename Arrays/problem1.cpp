cae4dedbd7fe1761c605cc084d49954c2ef8994e
Sanjeev Sharma <ssharma2418@outlook.com>
2023-07-14 09:31:36 +0530

    my-first-c++-programme

---
+++ Arrays/problem1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+
+
+  
+   int A[]={1,2,3,4,5};
+   
+  // int i;
+  // for(i=0; i<5; i++){
+  //   cout<<A[i]<<endl;
+  // } 
+  for(int x:A){
+    cout<<x<<endl;
+  }
+    return 0;
+}
